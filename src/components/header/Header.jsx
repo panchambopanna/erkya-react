866d17df55747f2a158b44d19cbd9d9d26a39fac
Karthikarya <aryakarthik13@gmail.com>
2022-05-11 15:14:11 +0530

    changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,33 +1,38 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import logo from '../../images/eryka space.png'
-import Button from '../button/Button'
-import Modal from '../modal/Modal'
-import './Header.css'
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../images/eryka space.png";
+import Button from "../button/Button";
+import Modal from "../modal/Modal";
+import "./Header.css";
 
 const Header = () => {
-    const [modal, setModal] = useState(false)
-    const [mType, setmType] = useState("")
-    const handleClick = (mType) => {
-        setModal(true)
-        setmType(mType)
-    }
+  const [modal, setModal] = useState(false);
+  const [mType, setmType] = useState("");
+  const handleClick = (mType) => {
+    setModal(true);
+    setmType(mType);
+  };
 
-    return (
-        <>
-            <div className='Header'>
-                <Link to='/'> <img className='Header__img' src={logo} alt="Eryka Kreations" /></Link >
-                <input className='Header__search' type="search" placeholder='Search for Creators' />
-                <div className="Header__buttons">
-                    <Button text="Log in" fn={() => handleClick("login")} />
-                    <Button text="Sign Up" color="blue" fn={() => handleClick("signUp")} />
-                </div>
-            </div >
+  return (
+    <>
+      <div className="header">
+        <Link to="/">
+          {" "}
+          <img className="header__img" src={logo} alt="Eryka Kreations" />
+        </Link>
+        <div className="header__searchbar">
+          <i class="fas fa-search"></i>
+          <input type="search" placeholder="Search for Creators" />
+        </div>
+        <div className="header__buttons">
+          <Button text="Log In" fn={() => handleClick("login")} />
+          <Button text="Sign Up" fn={() => handleClick("signUp")} />
+        </div>
+      </div>
 
-            {modal && <Modal mType={mType} setModal={setModal} setmType={setmType} />}
-        </>
-    )
-}
+      {modal && <Modal mType={mType} setModal={setModal} setmType={setmType} />}
+    </>
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/Makeup.jsx
+++ src/pages/Makeup.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Makeup = () => {
-    return (
-        <div className='Makeup container'>Makeup</div>
-    )
-}
+  return <div className="Makeup container">kjdgkjdhgiudfhdjkgjkd</div>;
+};
 
-export default Makeup
+export default Makeup;
