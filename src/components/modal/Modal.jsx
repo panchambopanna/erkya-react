6f6679e18c40b9949a0dc19df3d49cb7e8970908
Pancham Bopanna D <67087152+panchambopanna@users.noreply.github.com>
2022-08-03 12:23:55 +0530

    Forget password modal added

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -10,9 +10,14 @@ const Modal = ({ mType, setModal, setmType }) => {
   const handleLogin = () => {
     setmType("login");
   };
-  return (
-    <div className="modal">
-      {mType === "login" ? (
+  const handleFpass = () => {
+    setmType("fpass");
+    console.log(mType);
+  };
+
+  if (mType === "login") {
+    return (
+      <div className="modal">
         <div className="modal__content modal__login">
           <h2>LogIn</h2>
           <span className="close-btn" onClick={() => setModal(false)}>
@@ -44,7 +49,9 @@ const Modal = ({ mType, setModal, setmType }) => {
               <i class="fa fa-eye-slash ShowHidePw"></i>
             </div>
 
-            <div className="forgotpass">Forgot password?</div>
+            <div className="forgotpass" onClick={handleFpass}>
+              Forgot password?
+            </div>
 
             <Button text="LogIn" color="blue" />
 
@@ -67,7 +74,11 @@ const Modal = ({ mType, setModal, setmType }) => {
             </span>
           </div>
         </div>
-      ) : (
+      </div>
+    );
+  } else if (mType === "signUp") {
+    return (
+      <div className="modal">
         <div className="modal__content modal__signUp">
           <h2>Sign Up</h2>
           <span className="close-btn" onClick={() => setModal(false)}>
@@ -131,9 +142,71 @@ const Modal = ({ mType, setModal, setmType }) => {
             </span>
           </div>
         </div>
-      )}
-    </div>
-  );
+      </div>
+    );
+  } else if (mType === "fpass") {
+    return (
+      <div className="modal">
+        <div className="modal__content modal__fpass">
+          <h2>LogIn</h2>
+          <span className="close-btn" onClick={() => setModal(false)}>
+            {" "}
+            &times;
+          </span>
+          <form>
+            <div className="input-container">
+              <input
+                className="email-input"
+                type="email"
+                id="logemail"
+                placeholder=""
+              />
+              <label className="email-label" htmlFor="logemail">
+                Email
+              </label>
+            </div>
+
+            <div className="sendMail">
+              Enter your email address and a link will be sent to set new
+              password
+            </div>
+
+            <Button text="Verify" color="blue" />
+            {/* Add logic to send links or some reset logic */}
+
+            <div className="glogincontainer">
+              <div className="glogin">
+                <img src={google} alt="Google" />
+                <span>LogIn with Google</span>
+              </div>
+            </div>
+          </form>
+          <div className="loginlink">
+            Already Eryka User?{" "}
+            <span
+              onClick={() => {
+                handleLogin();
+              }}
+            >
+              {" "}
+              Log In{" "}
+            </span>
+          </div>
+          <div className="signuplink">
+            Not a Eryka User?{" "}
+            <span
+              onClick={() => {
+                handleSignUp();
+              }}
+            >
+              {" "}
+              Sign Up{" "}
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Modal;
